34fd018aa49895e7a9ea8e7d8a415166c6b78ae5
schittlur <78377530+schittlur@users.noreply.github.com>
2024-08-13 15:33:24 -0400

    menu modifications

---
+++ src/pages/talentFinder.js
@@ -0,0 +1,10 @@
+export default function Search() {
+    // const router = useRouter();
+    // const config = useConfig();
+
+    return (
+        <div>
+            test
+        </div>
+    );
+}
