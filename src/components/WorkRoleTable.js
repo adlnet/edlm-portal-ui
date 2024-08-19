cf08fb1adf49851c7acc5d0b1fd489fceca2a9d2
schittlur <78377530+schittlur@users.noreply.github.com>
2024-08-19 16:33:50 -0400

    filters page

--- src/components/WorkRoleTable.js
+++ src/components/WorkRoleTable.js
@@ -1,6 +1,5 @@
 import Button from "./Button";
 
-
 export function WorkRoleTable() {
 
     const data = [
@@ -168,17 +167,6 @@ export function WorkRoleTable() {
                     </li>
                 </ul>
             </nav>
-        </div>
-        <div className="flex justify-end mt-8">
-            <Button children={
-                <div className="flex flex-row gap-2">  
-                <p className="pt-0.5"> Select </p>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-                </svg>
-                </div>
-                
-            } />
         </div>
     </div>
     </>
