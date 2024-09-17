bd6777e49db3944ccf990c6540e48712b103cef7
schittlur <78377530+schittlur@users.noreply.github.com>
2024-09-17 17:33:40 -0400

    compare graphs

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,7 +1,10 @@
 // local host endpoint from .env
 export const backendHost = process.env.NEXT_PUBLIC_BACKEND_HOST;
-const api = '/api/';
+const api = 'api/';
 const elasticApi = '/es-api/';
+
+//graphs
+export const graph = `${backendHost}${api}graph/`;
 
 // // authentication urls
 // export const authLogin = `${backendHost}${api}auth/login`;
@@ -10,27 +13,6 @@ const elasticApi = '/es-api/';
 
 // // configuration url
 // export const configUrl = `${backendHost}${api}ui-configuration/`;
-
-// // spotlight courses url
-// export const spotlightCourses = `${backendHost}${api}spotlight-courses`;
-
-// // all the interest lists
-// export const interestLists = `${backendHost}${api}interest-lists/`;
-// export const userOwnedLists = `${backendHost}${api}interest-lists/owned`;
-
-// // course url
-// export const courseUrl = `${backendHost}${api}experiences/`;
-
-// // search url
-// export const searchUrl = `${backendHost}${elasticApi}`;
-
-// // More like this
-// export const moreLikeThisUrl = `${backendHost}${elasticApi}more-like-this/`;
-
-// // Save Search
-// export const saveSearchUrl = `${backendHost}${api}saved-filters`;
-// export const saveSearchOwnedUrl = `${backendHost}${api}saved-filters/owned`;
-
 
 // //Notifications
 // export const allNotification = `${backendHost}/inbox/notifications/api/all_list/`;
---
+++ src/pages/talentFinder/filters/alignment/compare.js
@@ -0,0 +1,75 @@
+// "use client";
+
+import Image from "next/image";
+import image from  "@/public/Picture1.png"
+import { useRouter } from 'next/router';
+import Button from "@/components/Button";
+import { WorkforceAlignmentTable } from "@/components/tables/WorkforceAlignmentTable";
+import DefaultLayout from "@/components/layouts/DefaultLayout";
+import { HomeIcon } from "@heroicons/react/outline";
+import { backendHost, graph } from '@/config/endpoints';
+import { useEffect, useState } from "react";
+import { axiosInstance } from "@/config/axiosConfig";
+
+
+export default function TalentFinderAlignment() {
+    const router = useRouter();
+    // const config = useConfig();
+    const [data, setData] = useState(null);
+
+
+    useEffect(() => {
+        axiosInstance
+        .get(graph)
+        .then((res) => {
+          setData(res.data);
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, []);
+
+    return (
+        <DefaultLayout>
+            <h2 className="flex w-5/6 text-4xl font-bold mt-8">Talent Finder</h2>
+            <div className="my-3 flex flex-row">
+                <a href={"/"} >
+                    <HomeIcon className="w-5 mx-2"/>  
+                </a>
+                &gt; 
+                <a href='/talentFinder' className="px-2 hover:mouse hover:underline hover:font-bold">
+                    Talent Finder
+                </a>
+                &gt; 
+                <a href='/talentFinder/filters' className="px-2 hover:mouse hover:underline hover:font-bold">
+                    Filters
+                </a>
+                &gt;
+                <a href='/talentFinder/filters/alignment' className="px-2 hover:mouse hover:underline hover:font-bold">
+                    Alignment Results
+                </a>
+                &gt; 
+                <p className="font-bold pl-2"> Compare Results</p>
+            </div>
+
+            <div className='bg-white shadow-md w-1/2 p-5 w-full mb-5'> 
+              <div className='pt-2 text-lg font-bold'> Compare Talent Across the Workforce </div>
+              <div className='pt-2 text-gray-600 pb-8'> Compare qualified individuals at the speed of relevance. </div>
+
+
+              <div className="flex justify-end mt-8">
+                    <Button children={
+                        <div className="flex flex-row gap-2">  
+                        <p className="pt-0.5"> Compare </p>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                        </svg>
+                        </div>
+                        
+                    } onClick={()=>router.push("/trainingPlan")}/>
+                </div>
+            </div>
+        </DefaultLayout>
+    );
+}
