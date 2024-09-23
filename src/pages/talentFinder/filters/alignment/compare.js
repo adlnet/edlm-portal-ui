67f9bd16893244f0cf4ef707dc5bbbf3e8d09688
JDTobin <90791836+JDTobin@users.noreply.github.com>
2024-09-23 10:41:41 -0400

    use iframe for graph

--- src/pages/talentFinder/filters/alignment/compare.js
+++ src/pages/talentFinder/filters/alignment/compare.js
@@ -15,47 +15,7 @@ import { axiosInstance } from "@/config/axiosConfig";
 
 export default function TalentFinderAlignment() {
     const router = useRouter();
-    //     axiosInstance
-    //     .get("https://edlmportal-admin.deloitteopenlxp.com/api/graph/?users="+users.join("&users="))
-    //     .then(resp => {
-    //         // safely(function() {
-    //         //     (function(root) {
-    //         //       function embed_document(root) {
-    //         //       const docs_json = {"4e5c17ac-4434-4401-a4ff-03ac1eb442ed":resp.data};
-    //         //       const render_items = [{"docid":"4e5c17ac-4434-4401-a4ff-03ac1eb442ed","roots":{"p1004":"testPlot"},"root_ids":["p1004"]}];
-    //         //       root.Bokeh.embed.embed_items(docs_json, render_items);
-    //         //       }
-    //         //       if (root.Bokeh !== undefined) {
-    //         //         embed_document(root);
-    //         //       } else {
-    //         //         let attempts = 0;
-    //         //         const timer = setInterval(function(root) {
-    //         //           if (root.Bokeh !== undefined) {
-    //         //             clearInterval(timer);
-    //         //             embed_document(root);
-    //         //           } else {
-    //         //             attempts++;
-    //         //             if (attempts > 100) {
-    //         //               clearInterval(timer);
-    //         //               console.log("Bokeh: ERROR: Unable to run BokehJS code because BokehJS library is missing");
-    //         //             }
-    //         //           }
-    //         //         }, 10, root)
-    //         //       }
-    //         //     })(window);
-    //         //   });
-    //         const all_json = resp.data;
-    //         const root_id = all_json.root_id;
-    //         const docs_json = {"4e5c17ac-4434-4401-a4ff-03ac1eb442ed":all_json.doc};
-    //         const render_items = [{"docid":"4e5c17ac-4434-4401-a4ff-03ac1eb442ed","roots":{[root_id]:"testPlot"},"root_ids":[root_id]}];
-    //         // embed_items(docs_json, render_items);
-    //     }
-    //         // embed_item(resp.data, 'testPlot')
-    //     )
-    //     .catch((err) => {
-    //       console.log(err);
-    //     });
-    // }, []);
+    const [iframeURL, setIframeURL] = useState("https://edlmportal-admin.deloitteopenlxp.com");
 
     useEffect(() => {
         axiosInstance
@@ -87,11 +47,11 @@ export default function TalentFinderAlignment() {
             //       }
             //     })(window);
             //   });
-            const all_json = resp.data;
-            const root_id = all_json.root_id;
-            const docs_json = {"4e5c17ac-4434-4401-a4ff-03ac1eb442ed":all_json.doc};
-            const render_items = [{"docid":"4e5c17ac-4434-4401-a4ff-03ac1eb442ed","roots":{[root_id]:"testPlot"},"root_ids":[root_id]}];
-            embed_items(docs_json, render_items);
+            setIframeURL("https://edlmportal-admin.deloitteopenlxp.com" + resp.data);
+            // const root_id = all_json.root_id;
+            // const docs_json = {"4e5c17ac-4434-4401-a4ff-03ac1eb442ed":all_json.doc};
+            // const render_items = [{"docid":"4e5c17ac-4434-4401-a4ff-03ac1eb442ed","roots":{[root_id]:"testPlot"},"root_ids":[root_id]}];
+            // embed_items(docs_json, render_items);
         }
             // embed_item(resp.data, 'testPlot')
         )
@@ -143,10 +103,10 @@ export default function TalentFinderAlignment() {
               <div className='pt-2 text-lg font-bold'> Compare Talent Across the Workforce </div>
               <div className='pt-2 text-gray-600 pb-8'> Compare qualified individuals at the speed of relevance. </div>
 
-              {/* <div id='testPlot' className="bk-root"></div> */}
-                <div className='flex justify-center'>
+              <div id='testPlot' className="flex justify-center"><iframe src={iframeURL} height={600} width={600}></iframe></div>
+                {/* <div className='flex justify-center'>
                     <Image src={graphImage} height={500} alt='' className='m-5 '/>
-                </div>
+                </div> */}
 
               <div className="flex justify-between gap-4 mt-8">
                     <Button children={
