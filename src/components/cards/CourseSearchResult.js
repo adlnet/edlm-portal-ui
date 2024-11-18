956ae3368c390edbf352e6381ce4c6af58f92fd4
Eric Chang <echang3@deloitte.com>
2024-11-17 23:10:30 -0700

    Migrate latest search results from xds0ui with updated config settings

--- src/components/cards/CourseSearchResult.js
+++ src/components/cards/CourseSearchResult.js
@@ -1,15 +1,28 @@
 'use strict';
-
 import { removeHTML } from '@/utils/cleaning';
 import { useAuth } from '@/contexts/AuthContext';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useRouter } from 'next/router';
 import { xAPISendStatement } from '@/utils/xapi/xAPISendStatement';
 import SaveModal from '@/components/modals/SaveModal';
+import { useConfig } from '@/hooks/useConfig';
+import { getDeeplyNestedData } from '@/utils/getDeeplyNestedData';
+import ShareButton from '@/components/buttons/ShareBtn';
+// import { Corner } from '@bokeh/bokehjs/build/js/lib/models/common/box_kinds';
 
 export default function SearchResult({ result }) {
   const { user } = useAuth();
   const router = useRouter();
+  const config = useConfig();
+
+  const title = useMemo(() => {
+    return (getDeeplyNestedData(config.data?.course_information?.course_title, result));
+  }, [config.isSuccess, config.data]);
+
+  const competencies = useMemo(() => {
+    return (getDeeplyNestedData(config.data?.course_information?.course_competency, result));
+  }, [config.isSuccess, config.data]);
+
 
   const handleClick = useCallback(() => {
     // create the context
@@ -24,8 +37,8 @@ export default function SearchResult({ result }) {
       },
       object: {
         id: `${window.origin}/learner/course/${result.meta.id}`,
-        definitionName: result.Course.CourseTitle,
-        description: result.Course.CourseShortDescription,
+        definitionName: title || result.Course.CourseTitle,
+        description: removeHTML(getDeeplyNestedData(config.data?.course_information?.course_description, result)) || result.Course.CourseShortDescription,
       },
       resultExtName: 'https://w3id.org/xapi/ecc/result/extensions/CourseId',
       resultExtValue: result.meta.id,
@@ -38,24 +51,40 @@ export default function SearchResult({ result }) {
   return (
     <div
       className='group hover:text-blue-400 hover:text-shadow cursor-pointer pr-2 pl-1 py-1 rounded-md outline-none focus-within:ring-2 focus-within:ring-blue-500'
-      title={result.Course.CourseTitle}
+      title={title || result.Course.CourseTitle}
     >
       <div className='flex justify-between items-center'>
         <button
           className='text-lg font-semibold group-hover:underline w-full text-left focus:outline-none'
           onClick={handleClick}
         >
-          <h3>{result.Course.CourseTitle}</h3>
+          <h3>{title || result.Course.CourseTitle}</h3>
         </button>
-        {user && <SaveModal courseId={result.meta.id} title={result.Course.CourseTitle} />}
+        <ShareButton
+                id={result.meta.id}
+                courseTitle={title || result.Course.CourseTitle}
+                courseDescription={removeHTML(getDeeplyNestedData(config.data?.course_information?.course_description, result)) || removeHTML(result.Course.CourseShortDescription)}
+        />
+        {user && <SaveModal courseId={result.meta.id} title={title || result.Course.CourseTitle} />}
       </div>
       <div onClick={handleClick} className='text-left' aria-hidden='true'>
         <h4>
           <strong>Provider:&nbsp;</strong>
-          {result.Course.CourseProviderName}
+          {getDeeplyNestedData(config.data?.course_information?.course_provider, result) || result.Course.CourseProviderName}
         </h4>
+        <h4>
+          <strong>Estimated Time: &nbsp;</strong>
+          { getDeeplyNestedData(config.data?.course_information?.course_time, result)  || 'Not available' }
+        </h4>
+        <h4>
+          <strong>Delivery Method: &nbsp;</strong>
+          { getDeeplyNestedData(config.data?.course_information?.course_deliveryMode, result) || 'Not available' }
+        </h4>
+        <div>
+          { competencies || 'Not available' }
+        </div>
         <p className='line-clamp-4 pr-4'>
-          {removeHTML(result.Course.CourseShortDescription)}
+          {removeHTML(getDeeplyNestedData(config.data?.course_information?.course_description, result)) ||removeHTML(result.Course.CourseShortDescription)}
         </p>
       </div>
     </div>
