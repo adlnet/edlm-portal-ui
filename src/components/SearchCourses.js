e8e7bbe8847dea9e6803d1831a711bc1f136e80d
Eric Chang <echang3@deloitte.com>
2024-11-21 16:23:00 -0700

    Update course search logic and similar course

--- src/components/SearchCourses.js
+++ src/components/SearchCourses.js
@@ -8,6 +8,7 @@ import { useCourseSearch} from '@/hooks/useCourseSearch';
 import MoreLikeThis from '@/components/cards/MoreLikeThis';
 import SearchResult from '@/components/cards/CourseSearchResult';
 import { useEffect } from 'react';
+import ContentLoadingAnimate from '@/utils/ContentLoadingAnimate';
 
 export default function SearchCourses( { params, setParams, courseSearchTriggerd }) {
 
@@ -35,37 +36,42 @@ export default function SearchCourses( { params, setParams, courseSearchTriggerd
       router.push({ pathname: '/learner/search', query: modified }, undefined, {
         scroll: true,
       });
+    } if (isLoading) {
+      return <ContentLoadingAnimate />;
     }
-  
+
+
+    // if loading
+  if (isLoading) {
+    return <ContentLoadingAnimate />;
+  }
+
     return (
-      <div className='mt-10 pb-4'>
-        {/* {data?.total > 0 && (
-          <span className={'text-gray-400 italic pt-12 font-sans px-px'}>
-            About {data.total} results.
-          </span>
-        )} */}
-        <div className={'grid grid-cols-12 pt-2 gap-12 '}>
-          <div id='search-results' className={'col-span-8 grid gap-8 relative'}>
-            {data &&
-              data?.hits?.map((course) => (
-                <SearchResult result={course} key={course.meta.id} />
-              ))}
-            <div className='py-8 sticky bottom-0 bg-gradient-to-t from-gray-50 mb-8'>
-              {!isLoading && data && (
-                <Pagination
-                  totalPages={Math.ceil(
-                    data?.total / config?.data?.search_results_per_page
-                  )}
-                  handleSpecificPage={handleSpecificPage}
-                  currentPage={parseInt(params.p)}
-                />
-              )}
-            </div>
-            <div className='relative row-span-4'>
-              {data && data?.hits && <MoreLikeThis course={data?.hits[0]} />}
-            </div>
+      <div className='mt-4 pb-4'>
+        <div className='grid grid-cols-1 md:grid-cols-12 pt-0 gap-6 md:gap-12'>
+          <div id='search-results' className='col-span-12 grid gap-2 relative'>
+            {data && data?.hits?.map((course) => (     
+              <SearchResult result={course} key={course.meta.id} />    
+            ))}       
           </div>
+
+          <div className='col-span-1 md:col-span-12 flex flex-col justify-center w-full -mt-4 px-2 max-w-7xl mx-auto'>
+            <div className="text-[#1b1128] text-2xl font-bold leading-normal">Similar Courses</div>       
+              <div className='inline-flex overflow-x-auto gap-2 pb-4 py-2 custom-scroll'>    
+                {data && data?.hits && data?.hits?.map((course) => <MoreLikeThis course={course} key={course.meta.id} />)}  
+              </div>   
+            </div>   
         </div>
-      </div>
+        
+        <div className='py-3 sticky bottom-0 -mb-8'>     
+            {!isLoading && data && (     
+              <Pagination     
+                totalPages={Math.ceil(data?.total / config?.data?.search_results_per_page)}    
+                handleSpecificPage={handleSpecificPage}    
+                currentPage={parseInt(params.p)}     
+            />   
+          )}     
+        </div>  
+      </div>     
     );
 }
