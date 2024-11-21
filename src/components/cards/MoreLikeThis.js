42242cdc973cf8f416d3fe00503b64984cc32705
Eric Chang <echang3@deloitte.com>
2024-11-21 14:49:09 -0700

    Update similar courses for UI purposes

--- src/components/cards/MoreLikeThis.js
+++ src/components/cards/MoreLikeThis.js
@@ -3,117 +3,61 @@
 import { removeHTML } from '@/utils/cleaning';
 import { useAuth } from '../../contexts/AuthContext';
 import { useMoreCoursesLikeThis } from '../../hooks/useMoreCoursesLikeThis';
-import React, { useMemo } from 'react';
-import SaveModal from '../modals/SaveModal';
-import ViewBtn from '../buttons/ViewBtn';
+import { useMemo, useCallback } from 'react';
 import { useConfig } from '@/hooks/useConfig';
 import { getDeeplyNestedData } from '@/utils/getDeeplyNestedData';
+import { xAPISendStatement } from '@/utils/xapi/xAPISendStatement';
+import Link from 'next/link';
+import ContentLoadingAnimate from '@/utils/ContentLoadingAnimate';
 
 export default function MoreLikeThis({ course }) {
   const { data, isLoading } = useMoreCoursesLikeThis(course?.meta.id);
+  const { Course, meta, Technical_Information, Course_Instance } = {
+    ...course,
+  };
   const { user } = useAuth();
   const config = useConfig();
 
-    // prepare the course data
-    const preppedData = useMemo(() => {
-      if (!config.isSuccess) return null;
-      return {
-        title: removeHTML(
-          getDeeplyNestedData(
-            config.data?.course_information?.course_title, course)
-        ),
-        date: {
-          start: getDeeplyNestedData(
-            config.data?.course_information?.course_startDate,
-            course
-          )?.replace(' ', '').split('T')[0],
-          end: getDeeplyNestedData(
-            config.data?.course_information?.course_endDate,
-            course
-          )?.replace(' ', '').split('T')[0],
-        },
-        description: removeHTML(
-          getDeeplyNestedData(config.data?.course_information?.course_description, course)
-        ),
-        type: removeHTML(
-          getDeeplyNestedData(config.data?.course_information?.course_type, course)
-        ),
-        time: removeHTML(
-          getDeeplyNestedData(config.data?.course_information?.course_time, course)
-        ),
+  const title = useMemo(() => {
+    return getDeeplyNestedData(config.data?.course_information?.course_title, course);
+  }, [config.isSuccess, config.data]);
 
-        url: getDeeplyNestedData(
-          config.data?.course_information?.course_url,
-          course
-        ),
-        code: getDeeplyNestedData(config.data?.course_information?.course_code, course),
-        photo:
-          getDeeplyNestedData('Course_Instance.Thumbnail', course) ||
-          getDeeplyNestedData(config.data?.course_information?.course_thumbnail, course),
-  
-        provider: getDeeplyNestedData(config.data?.course_information?.course_provider, course),
-        instructor: getDeeplyNestedData(
-          config.data?.course_information?.course_instructor,
-          course
-        ),
-        delivery: getDeeplyNestedData(
-          config.data?.course_information?.course_deliveryMode,
-          course
-        ),
-        details: config.data?.course_highlights?.map((highlight) => {
-          return {
-            title: highlight.display_name,
-            content: removeHTML(
-              getDeeplyNestedData(highlight.field_name, course)
-            ),
-          };
-        }),
+  const provider = useMemo(() => {
+    return getDeeplyNestedData(config.data?.course_information?.course_provider, course);
+  }, [config.isSuccess, config.data]);
+
+  const handleClick = useCallback(
+    (e) => {
+      if (!user)
+        return router.push(`/learner/course/${meta.metadata_key_hash || meta.id}`);
+
+      const context = {
+        actor: {
+          first_name: user?.user?.first_name,
+          last_name: user?.user?.last_name,
+        },
+        verb: {
+          id: 'https://w3id.org/xapi/tla/verbs/explored',
+          display: 'explored',
+        },
+        object: {
+          id: `${window.origin}/learner/course/${meta.id}`,
+          definitionName: Course.CourseTitle,
+          description: Course.CourseShortDescription,
+        },
+        resultExtName: 'https://w3id.org/xapi/ecc/result/extensions/CourseId',
+        resultExtValue: meta.metadata_key_hash || meta.id,
       };
-    }, [course?.isSuccess, course?.data, config.isSuccess, config.data]);
+      xAPISendStatement(context);
+      router.push('/learner/course/' + (meta.metadata_key_hash || meta.id));
+    },
+    [Course, meta, user]
+  );
 
   // if loading
   if (isLoading) {
-    return (
-      <div className='animate-pulse'>
-        <div className='w-full bg-white border rounded-md border-gray-200 p-4'>
-          <div className='w-3/4 bg-gray-200 rounded-sm h-8'></div>
-          <div className='bg-gray-200 h-32 rounded-sm mt-4'></div>
-          <div className='grid grid-cols-5 gap-2 mt-4'>
-            <div className='col-span-1 rounded-sm bg-gray-200 h-4'></div>
-            <div className='col-span-2 rounded-sm bg-gray-200'></div>
-          </div>
-          <div className='grid grid-cols-5 gap-2 mt-2'>
-            <div className='col-span-1 rounded-sm bg-gray-200 h-4'></div>
-            <div className='col-span-2 rounded-sm bg-gray-200'></div>
-          </div>
-          <div className='grid grid-cols-5 gap-2 mt-2'>
-            <div className='col-span-1 rounded-sm bg-gray-200 h-4'></div>
-            <div className='col-span-2 rounded-sm bg-gray-200'></div>
-          </div>
-          <div className='grid grid-cols-5 gap-2 mt-2'>
-            <div className='col-span-1 rounded-sm bg-gray-200 h-4'></div>
-            <div className='col-span-2 rounded-sm bg-gray-200'></div>
-          </div>
-          <div className='flex justify-between mt-8'>
-            <div className='inline-flex justify-start gap-2'>
-              <div className='rounded-full h-6 w-6 bg-gray-200'></div>
-              <div className='rounded-full h-6 w-6 bg-gray-200'></div>
-            </div>
-            <div className='rounded-l-2xl rounded-r-md h-6 w-10 bg-gray-200'></div>
-          </div>
-        </div>
-
-        <div className='flex justify-center gap-2 mt-2'>
-          <div className='bg-gray-200 rounded-full border border-gray-300 h-2 w-2'></div>
-          <div className='bg-gray-200 rounded-full border border-gray-300 h-2 w-2'></div>
-          <div className='bg-gray-200 rounded-full border border-gray-300 h-2 w-2'></div>
-          <div className='bg-gray-200 rounded-full border border-gray-300 h-2 w-2'></div>
-          <div className='bg-gray-200 rounded-full border border-gray-300 h-2 w-2'></div>
-        </div>
-      </div>
-    );
+    return <ContentLoadingAnimate />;
   }
-
   // if error
   else if (data.hits.length < 1) {
     return null;
@@ -121,46 +65,28 @@ export default function MoreLikeThis({ course }) {
 
   // show suggested card
   return (
-    <div>
-      <span className={'text-gray-400 italic block pb-5 font-sans px-px'}>
-        Similar Course
-      </span>
-      <div className='w-full bg-white border rounded-md border-gray-200 p-4 shadow'>
-        <h1 className='text-lg font-semibold'>
-          {preppedData?.title || data.hits[0].Course.CourseTitle}
-        </h1>
-        <p className='mt-4 font-sans line-clamp-6 text-sm'>
-          {preppedData?.description || removeHTML(data.hits[0].Course.CourseShortDescription)}
-        </p>
-        <div className='flex flex-col gap-1 mt-4'>
-          <div>
-            <span className='font-semibold'>Course Code:&nbsp;</span>
-            {preppedData?.code || data.hits[0].Course?.CourseCode || 'Not Available'}
+    <Link href={`/learner/course/${meta.metadata_key_hash || meta.id}`} passHref>
+      <div
+        onClick={handleClick}
+        role='button'
+        tabIndex='0'
+        aria-hidden='true'
+        className="px-5 py-[19px] bg-gray-500 rounded-lg shadow flex-col justify-start items-start gap-2.5 inline-flex cursor-pointer transform transition-shadow duration-150 ease-in-out font-['Roboro'] text-white h-[228px] w-[387px] hover:shadow-lg"
+      >
+        <div className="w-[330px] relative">
+          <div className="w-full flex flex-col">
+            <div className="text-white text-xl font-bold font-['Roboto'] leading-normal">
+              {title || Course?.CourseTitle}
+            </div>
           </div>
           <div>
-            <span className='font-semibold'>Course Type:&nbsp;</span>
-            {preppedData?.type || data.hits[0].Course?.CourseType || 'Not Available'}
+            <span className="text-white text-base font-medium font-['Roboto'] leading-normal">Provider: </span>
+            <span className="text-white text-base font-medium font-['Roboto'] leading-normal">
+              {provider || Course?.CourseProviderName}
+            </span>
           </div>
-          <div>
-            <span className='font-semibold'>Estimated Time:&nbsp;</span>
-            {preppedData.time || data.hits[0].Course?.EstimatedCompletionTime || 'Not Available'}
-          </div>
-          <div>
-            <span className='font-semibold'>Course Provider:&nbsp;</span>
-            {preppedData.provider || data.hits[0].Course?.CourseProviderName || 'Not Available'}
-          </div>
-        </div>
-        <div className='flex justify-between mt-10'>
-          <div className='flex gap-2'>
-            <ViewBtn
-              id={data.hits[0].meta.id}
-              courseTitle={preppedData?.title}
-              courseDescription={preppedData?.description}
-            />
-          </div>
-          {user && <SaveModal courseId={data.hits[0].meta.id} />}
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
