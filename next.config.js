31c93bed7e59e38f73b948220ea7d8567ee057b7
Simeranjeet Sandhu <simeransandhu@deloitte.com>
2025-01-27 12:57:02 -0800

    Added next config basePath/Modified backend env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,64 @@
-// next.config.js
+// const { createSecureHeaders } = require("next-secure-headers");
 
-module.exports = {
-    reactStrictMode: false,
+const nextConfig = {
+    reactStrictMode: true,
     eslint: {
         ignoreDuringBuilds: false,
     },
+    swcMinify: true,
+    basePath: '/edlm-portal',
+    
+    // Adding policies:
+    // async headers() {
+    //     return [
+    //         {
+    //             source: '/(.*)',
+    //             headers: createSecureHeaders({
+    //                 contentSecurityPolicy: {
+    //                     directives: {
+    //                         defaultSrc: [
+    //                             "'self'",
+    //                             "https://ecc.staging.dso.mil",
+    //                             "https://ecc.staging.dso.mil/ecc-openlxp-xms",
+    //                             "https://ecc.staging.dso.mil/ecc-openlxp-xms-ui/", 
+    //                             "https://ecc.apps.dso.mil/",
+    //                             "https://ecc.apps.dso.mil/ecc-openlxp-xms-ui/",
+    //                             "https://fonts.googleapis.com"
+    //                         ],
+    //                         styleSrc: [
+    //                             "'self'",
+    //                             "https://ecc.staging.dso.mil",
+    //                             "https://ecc.staging.dso.mil/ecc-openlxp-xms",
+    //                             "https://ecc.staging.dso.mil/ecc-openlxp-xms-ui/", 
+    //                             "https://ecc.apps.dso.mil/",
+    //                             "https://ecc.apps.dso.mil/ecc-openlxp-xms-ui/",
+    //                             "https://fonts.googleapis.com"
+    //                         ],
+    //                         imgSrc: ["'self'",
+    //                                 "data:",
+    //                                 "data:*",
+    //                         ],
+    //                         fontSrc: [
+    //                             "'self'", 
+    //                             "https://fonts.gstatic.com"
+    //                         ],
+    //                         frameAncestors: [
+    //                             "'self'",
+    //                             "https://ecc.apps.dso.mil/",
+    //                             "https://ecc.apps.dso.mil/ecc-openlxp-xms-ui/",
+    //                             "https://ecc.staging.dso.mil/ecc-openlxp-xms-ui/"
+    //                         ]
+    //                     },
+    //                     frameGuard: "deny",
+    //                     noopen: "noopen",
+    //                     nosniff: "nosniff",
+    //                     xssProtection: "sanitize",
+    //                     referrerPolicy: "origin-when-cross-origin",
+    //                 }
+    //             })
+    //         },
+    //     ];
+    // },
 }
+
+module.exports = nextConfig
