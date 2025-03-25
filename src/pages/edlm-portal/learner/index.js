25ec0730ef0f07f8653237076f28b864dd5f4aa2
Eric Chang <echang3@deloitte.com>
2025-03-25 16:40:56 -0700

    Update login & register path

--- src/pages/edlm-portal/learner/index.js
+++ src/pages/edlm-portal/learner/index.js
@@ -235,7 +235,7 @@ export default function Home() {
               ]}
               mobileBreakpoint={670}
             >
-              {spotlight?.data != [] && spotlight.data?.map((course) => {
+              {spotlight && spotlight.data?.map((course) => {
                 return(
                   <Carousel.Item key={course.id}>
                     <CourseSpotlightCarouselCard course={course} key={course.meta.id} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 import { useAuth } from '@/contexts/AuthContext';
-import Home from './edlm-portal/learner/index';
-import Login from './login';
+import Home from '@/pages/edlm-portal/learner/index';
+import Login from '@/pages/edlm-portal/login';
 
 export default function IntialPage() {
   const { user } = useAuth();
