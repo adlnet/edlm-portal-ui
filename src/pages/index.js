32f5f9bce1ab49509d53b6b78ba8103698ddcdfc
Eric Chang <echang3@deloitte.com>
2025-03-27 23:28:06 -0700

    Add root redirect

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function RootRedirect() {
+  const router = useRouter();
+  
+  useEffect(() => {
+    router.replace('/edlm-portal');
+  }, [router]);
+  
+  return <div>Redirecting...</div>;
+}
