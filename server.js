962088b6ec8a951be89d597195a97074f6289a67
Eric Chang <echang3@deloitte.com>
2025-04-01 23:31:28 +0000

    Revert "Update for comptency chart"
    This reverts commit a5c28f5258197e48eb8d5653150117d160dfe743

--- server.js
+++ server.js
@@ -10,12 +10,8 @@ app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     const { pathname } = parsedUrl;
-
-    if (pathname.includes('/edlm-portal/edlm-portal/')) {
-      const newPath = pathname.replace('/edlm-portal/edlm-portal/', '/edlm-portal/');
-      parsedUrl.pathname = newPath;
-
-    } else if (pathname.startsWith('/edlm-portal/_next/')) {
+    
+    if (pathname.startsWith('/edlm-portal/_next/')) {
       const newPath = pathname.replace('/edlm-portal/_next/', '/_next/');
       parsedUrl.pathname = newPath;
     }
