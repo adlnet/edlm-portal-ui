c0dda1fb64b2a69bb5d205f109b2c94865fa4c58
Eric Chang <echang3@deloitte.com>
2025-04-12 21:50:56 +0000

    Revert "Trying new setting"
    This reverts commit bcb6999fcbaa083e4792a6889f8ddd2baeab8c54

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
     },
     swcMinify: true,
     basePath: '/edlm-portal',
-    assetPrefix: '/',
     trailingSlash: true,
 
     images: {
@@ -17,11 +16,11 @@ const nextConfig = {
     async rewrites() {
         return [
             {
-                source: '/api/data/:type/:uuid',
+                source: '/edlm-portal/api/data/:type/:uuid',
                 destination: 'http://cass.cass:80/api/data/:type/:uuid',
             },
             {
-                source: '/api/data/:path',
+                source: '/edlm-portal/api/data/:path',
                 destination: 'http://cass.cass:80/api/data/:path',
             }
         ]
