ecb36304c23e92bf80d2cb9efbd5b0704ae1ded1
Eric Chang <echang3@deloitte.com>
2025-04-12 23:59:47 +0000

    Revert "Trying update custom server seeting"
    This reverts commit e49e088f582ba1bd94e2f75e5b9d2ed1a89e7a87

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.prepare().then(() => {
     const parsedUrl = parse(req.url, true);
     const { pathname } = parsedUrl;
     
-    if (!pathname.startsWith('/edlm-portal')) {
+    if (pathname.startsWith('/_next')) {
       parsedUrl.pathname = '/edlm-portal' + pathname;
     }
     
