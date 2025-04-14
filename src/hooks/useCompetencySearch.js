07e05fd7f20d3ed713ed11132b2d1ca04c82fccf
Eric Chang <echang3@deloitte.com>
2025-04-14 16:11:21 -0700

    Update useComp logic to fetch all data in parallel and cache data for 1 hour

--- src/hooks/useCompetencySearch.js
+++ src/hooks/useCompetencySearch.js
@@ -2,19 +2,21 @@
 
 import { axiosInstance } from '@/config/axiosConfig';
 import { compSearchUrl } from '@/config/endpoints';
+import { oneHour } from '@/config/timeConstants';
 import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import axios from 'axios';
 import axiosRetry from 'axios-retry';
 import backupData from '@/public/backup_competencies.json';
 
 // Competency Object created to hold all necessary competency variables 
-function Competency(name, desc, id, parent, children){
-  this.name = name;
-  this.desc = desc;
-  this.id = id;
-  this.parent = parent;
-  this.children = children;
-}
+const Competency = (name, desc, id, parent, children) => ({
+  name,
+  desc,
+  id,
+  parent,
+  children
+});
 
 // To go through nextjs.proxy
 function proxyUrl(url) {
@@ -26,15 +28,16 @@ function proxyUrl(url) {
 
 // // Helper function to assign parent and children values 
 // //  based on the relationship links
-function getRelateLinks(relateLinks, competencies){
+async function getRelateLinks(relateLinks, competencies){
 
   // Creating the result to return with a starting value of the current competencies
-  const result = competencies;
+  const result = [...competencies];
+  const relationPromises = [];
 
   // Looping through every Relation Link to assign parent and children
   //  values to the competencies
   for (const key in relateLinks) {
-      axios.get(proxyUrl(relateLinks[key]))
+    const promise = axios.get(proxyUrl(relateLinks[key]))
         .then(res =>{
           
           // Response information
@@ -50,26 +53,29 @@ function getRelateLinks(relateLinks, competencies){
         .catch(error=>{
           console.log('Relate Error: ', error)
         })
+        relationPromises.push(promise);
   }
 
+  await Promise.all(relationPromises);
   return result
 }
 
 // Helper function that gets all the competency objects from the ECCR and 
 //  then sends them to getRelateLinks for proper parent and children assignments.
 //  Returns 
-function getCompData(compLinks){
+async function getCompData(compLinks){
 
   const result = []
+  const fetchPromises = [];
 
   // Function to fetch data from all the competency links and return once 
   //  all axios requests have completed
   const fetchData = async() => {
     for (const key in compLinks) {
-      await axios.get(proxyUrl(compLinks[key]))
+      const promise = axios.get(proxyUrl(compLinks[key]))
           .then(res=>{
 
-              let compInfo = res.data;
+              const compInfo = res.data;
 
               let name = compInfo.name["@value"];
               if (name === undefined){
@@ -80,7 +86,7 @@ function getCompData(compLinks){
                   desc = compInfo.description;
               }
 
-              let compObj = new Competency(name, desc, compLinks[key], '', [])
+              const compObj = Competency(name, desc, compLinks[key], '', [])
               result.push(compObj)
       
           })
@@ -88,25 +94,21 @@ function getCompData(compLinks){
               console.log('Comp Link Error: ', error);
           }) 
     } 
-    return result
+    fetchPromises.push(promise);
   };
-
+  await Promise.all(fetchPromises);
   return fetchData();
 }
 
-/** 
- * Hook to get the competency results
- * @returns all competencies from the DOT&E Framework defined in the Dev-ECCR
- */
+// Hekper function to fetch competency data
+async function getCompetencySearch() {
 
-export function useCompetencySearch() {
-
-  // // Setting up form data for API call
+  // Setting up form data for API call
   const FormData = require('form-data');
-  let data = new FormData();
+  const data = new FormData();
   data.append('signatureSheet', '[]');
   
-  let config = {
+  const config = {
     method: 'post',
     maxBodyLength: Infinity,
     url: compSearchUrl,
@@ -116,46 +118,42 @@ export function useCompetencySearch() {
     retryAfter: 500
   };
 
-  // Setting up return data
-  const [Data, setData]=useState({
-    Name:'',
-    Competencies:[]
-  })
-
   axiosRetry(axiosInstance, { 
     retries: 5, 
     shouldResetTimeout: true
   });
 
-  useEffect(() => {
-    // Making API request
-      axios.request(config)
-        .then(response=>{
+   try {
+    const response = await axios.request(config);
+    const compData = response.data;
+    
+    const relateLinks = compData.relation;
+    const compLinks = compData.competency;
+    
+    const competencyObjects = await getCompData(compLinks);
+    const competenciesWithRelations = await getRelateLinks(relateLinks, competencyObjects);
+    
+    return {
+      Name: compData.name?.["@value"] || "",
+      Competencies: competenciesWithRelations
+    };
+  } catch (error) {
+      console.log('Error on initial API request - using backup data: ', error)
+      return {
+        Competencies: backupData
+    }
+  };
+};
 
-          // Setting competency from response
-          let compData = response.data;
-          
-          let relateLinks = compData.relation;
-          let compLinks = compData.competency;
-
-          getCompData(compLinks)
-            .then( res => {
-              const allRelateData = getRelateLinks(relateLinks, res)
-
-              setData({
-                Name:compData.name["@value"],
-                Competencies: allRelateData
-              }) 
-            })
-
-        })
-        .catch(error=>{
-          console.log('Error on initial API request - using backup data: ', error)
-          setData({
-            Competencies: backupData
-          })
-        })
-  },[])
-
-  return Data.Competencies
+/** 
+ * Hook to get the competency results
+ * @returns all competencies from the DOT&E Framework defined in the ECCR
+ */
+export function useCompetencySearch() {
+  const { data, isLoading, error } = useQuery('competencies', getCompetencySearch, {
+    staleTime: oneHour,
+    cacheTime: oneHour,
+  });
+  
+  return data?.Competencies || [];
 }
