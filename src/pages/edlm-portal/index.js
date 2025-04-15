b8a113a52b874a6fc7316de67988509d544b54c8
Eric Chang <echang3@deloitte.com>
2025-04-15 07:13:05 -0700

    Put login back for dev instance

--- src/pages/edlm-portal/index.js
+++ src/pages/edlm-portal/index.js
@@ -1,13 +1,16 @@
 'use strict';
 
+import { useAuth } from '@/contexts/AuthContext';
 import Home from '@/pages/edlm-portal/learner/index';
+import Login from '@/pages/edlm-portal/login';
 
 export default function IntialPage() {
+  const { user } = useAuth();
 
   return (
     <>
-     {/* Dont redirect to login page on P1 */}
-     <Home />
+    {/* Dont redirect to login page on P1 */}
+     {user ? <Home /> : <Login />}
     </>
   );
 }
