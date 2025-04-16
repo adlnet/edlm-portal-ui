3ad228570dd8270a67aac8a33b47ab4657dc84f6
Eric Chang <echang3@deloitte.com>
2025-04-16 07:48:36 -0700

    Update add back login

--- src/pages/edlm-portal/index.js
+++ src/pages/edlm-portal/index.js
@@ -1,10 +1,15 @@
 'use strict';
+
+import { useAuth } from '@/contexts/AuthContext';
 import Home from '@/pages/edlm-portal/learner/index';
+import Login from '@/pages/edlm-portal/login';
+
 export default function IntialPage() {
+  const { user } = useAuth();
+
   return (
     <>
-     {/* Dont redirect to login page on P1 */}
-     <Home />
+     {user ? <Home /> : <Login />}
     </>
   );
 }
