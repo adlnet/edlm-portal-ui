2ef8711c09b3b3502f2ccaeb24b852058392aca9
Eric Chang <echang3@deloitte.com>
2025-04-21 10:11:13 -0700

    RM default login page, no portal login on P1

--- src/pages/edlm-portal/index.js
+++ src/pages/edlm-portal/index.js
@@ -1,16 +1,12 @@
 'use strict';
 
-import { useAuth } from '@/contexts/AuthContext';
 import Home from '@/pages/edlm-portal/learner/index';
-import Login from '@/pages/edlm-portal/login';
 
 export default function IntialPage() {
-  const { user } = useAuth();
 
   return (
     <>
-    {/* Dont redirect to login page on P1 */}
-     {user ? <Home /> : <Login />}
+     <Home />
     </>
   );
 }
