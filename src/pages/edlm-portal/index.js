909622fa0488cf50a92a1590e4e33569a6fc6079
Eric Chang <echang3@deloitte.com>
2025-04-22 09:27:22 -0700

    Update moodler session call

--- src/pages/edlm-portal/index.js
+++ src/pages/edlm-portal/index.js
@@ -13,9 +13,11 @@ export default function IntialPage() {
   // Get Moodle session
   useEffect(() => {
     // Get moodle session from the Moddle staging environment
+    // If already have moodle session, do not call the API again
+    // If not in the p1 environment, do not call the API
     if (isSessionInit.current) return;
     if (process.env.ENABLE_ASSET_PREFIX !== 'true') return;
-
+    
     isSessionInit.current = true;
     moodleSession.mutate(null, {
       onSuccess: (res) => {
