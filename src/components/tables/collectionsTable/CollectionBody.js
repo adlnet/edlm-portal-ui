a8c875d06982e2e96204ba867c70691173ec8a15
Eric Chang <echang3@deloitte.com>
2025-09-15 13:37:55 -0700

    Update table unclickable logic and competencies logic

--- src/components/tables/collectionsTable/CollectionBody.js
+++ src/components/tables/collectionsTable/CollectionBody.js
@@ -20,6 +20,15 @@ const TableBody = ({ pageData, columns, deleteCourse, url}) => {
             // Specific row creation for title in Collection tables 
             if (accessor === 'title'){
                 const tData = data[accessor] ? data[accessor] : "——";
+                
+                // This is used to handle unclickable rows
+                if (data.isUnClickable) {
+                    return (
+                        <td key={accessor} className='pl-4'>
+                            {tData}
+                        </td>
+                    );
+                }
 
                 const linkUrl = url
                     ? url(data)
@@ -39,6 +48,28 @@ const TableBody = ({ pageData, columns, deleteCourse, url}) => {
                 const tData = data[accessor] ? new Date(data[accessor]).toLocaleDateString() : "——"
                 return <td key={accessor} className='pl-4' >{tData}</td>;
             }
+
+            // Specific row creation for competencies in tables
+            else if (accessor === 'competencies'){
+                const competencies = data[accessor];
+                if (!competencies || competencies.length === 0) {
+                    return <td key={accessor} className='pl-4'>——</td>;
+                }
+                return (
+                    <td key={accessor} className='pl-4'>
+                        <div className="flex gap-2 flex-wrap">
+                            {competencies.map((comp) => (
+                                <div key={comp} className="w-auto h-7 px-[15px] py-1.5 bg-[#e5efff] rounded-xl justify-center items-center gap-2 flex">
+                                    <div className="text-center text-[#3892f3] text-sm font-normal font-['Roboto'] leading-tight whitespace-nowrap">
+                                        {comp}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </td>
+                );
+    }
+
             else{
                 const tData = data[accessor] ? data[accessor] : "——";
                 return <td key={accessor} className='pl-4' >{tData}</td>; 
