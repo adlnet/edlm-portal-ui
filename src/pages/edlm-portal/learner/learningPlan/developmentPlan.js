2a6c2b7511df783e8136f679f848db2ce81d5cc4
Eric Chang <echang3@deloitte.com>
2025-09-23 18:07:36 -0700

    Update development plan .js

--- src/pages/edlm-portal/learner/learningPlan/developmentPlan.js
+++ src/pages/edlm-portal/learner/learningPlan/developmentPlan.js
@@ -69,7 +69,7 @@ export default function DevelopmentPlan() {
                   <div className='flex flex-row'>
                     <Button 
                       className='flex justify-center bg-blue-900 hover:bg-blue-600 ml-2' 
-                      onClick={() =>{}}
+                      onClick={() => setCurrentStep(2)}
                     >
                       Start Role Based Plan
                     </Button>
