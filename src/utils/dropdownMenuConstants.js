e011fed0fe119d72d83633ba60855204169ae130
Eric Chang <echang3@deloitte.com>
2025-09-29 13:04:06 -0700

    Update menu constants and btn

---
+++ src/utils/dropdownMenuConstants.js
@@ -0,0 +1,54 @@
+'use strict';
+
+export const timeframeOptions = ['Short-term (1-2 years)', 'Long-term (3-4 years)'];
+export const proficiencyLevels = ['Basic', 'Intermediate', 'Advanced', 'Mastery'];
+export const priorityOptions = ['Highest', 'High', 'Medium', 'Low', 'Lowest'];
+export const shortTermGoalTimeLine = [
+    '1-3 months',
+    '3-6 months',
+    '6-9 months',
+    '9-12 months',
+    '12-18 months',
+    '18-24 months'
+];
+
+export const longTermGoalTimeLine = [
+    '2-2.5 years',
+    '2.5-3 years',
+    '3-3.5 years',
+    '3.5-4 years',
+    '4-4.5 years',
+    '4.5-5 years'
+];
+
+export const resourceSupportOptions = [
+    'Formal training courses or certifications',
+    'Stretch assignments or special projects',
+    'Online learning platforms and resources',
+    'External professional networking',
+    'Mentorship from senior colleagues',
+    'Professional conferences and workshops',
+    'Cross-functional team collaborations',
+    'Supervisor guidance and support',
+    'Other'
+];
+
+export const obstacleOptions = [
+    'Limited time due to current workload',
+    'Limited prior experience in this area',
+    'Limited availability of mentors/experts',
+    'Prerequisite skills or knowledge gaps',
+    'Budget constraints for traning/resources',
+    'Competing work priorities and deadlines',
+    'Need for additional organizational support',
+    'Access to necessary tools or technology',
+    'Other'
+];
+
+// Mock data for development
+// In the future, this data will come from an API
+export const ksaOptions = [
+    { name: 'Opposing Forces', description: 'In coordination with the Intelligence Community, maintains operational understanding' },
+    { name: 'System User', description: 'Understands the user mission/training and how the user will use and operate the DoD systems' },
+    { name: 'DoD Systems', description: 'Understands the DoD systems and its mission critical function including but not limited to sub-components' },
+];
