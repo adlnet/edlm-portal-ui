7b599859d97b963b5bc3ec43e450ef27f2de0232
mdonlon <mdonlon@deloitte.com>
2025-12-05 10:08:50 -0500

    cleaning up owned.js

--- src/pages/edlm-portal/learner/lists/owned.js
+++ src/pages/edlm-portal/learner/lists/owned.js
@@ -111,7 +111,6 @@ export default function Owned() {
     <div className='mt-7 pb-5'>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
           {isSuccess && currentCards?.map((cardItem) => (
-            console.log(cardItem),
             <CollectionCard
               key={cardItem.id}
               title={cardItem.name}
