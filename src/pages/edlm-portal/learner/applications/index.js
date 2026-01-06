c90a8ec011bfb8a91bfa0cecd11cb4925b34bf22
mdonlon <mdonlon@deloitte.com>
2026-01-06 11:30:00 -0500

    Application page creation

---
+++ src/pages/edlm-portal/learner/applications/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import DefaultLayout from "@/components/layouts/DefaultLayout";
+
+export default function Applications() {
+
+  return (
+    <DefaultLayout>
+      <div className='bg-white shadow-md py-4 px-4 my-4 rounded-xl mx-4 min-h-[700px]'>
+        <h1 className="text-xl font-bold text-blue-900 pb-4">My Applications</h1>
+        <div className="text-gray-600 pb-10"> Manage and track your certification applications.</div>
+        <button 
+         className="flex px-4 py-2 justify-center rounded-md text-white text-sm bg-[#23717E] hover:bg-teal-800"
+         onClick={()=>{}}
+        >
+          <div className="flex gap-2 items-center">
+            Start New Application 
+            <ArrowRightIcon className="w-3 h-3 stroke-[4]"/>
+          </div>
+        </button>
+       
+      </div>
+    </DefaultLayout>
+  );
+}
